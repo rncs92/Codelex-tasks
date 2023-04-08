dd6197f757eb8ca543c72eff95930906eecf577f
rncs92 <123461096+rncs92@users.noreply.github.com>
2023-04-08 10:49:38 +0300

    update

--- Tasks-Part2/OOP/exe08.php
+++ Tasks-Part2/OOP/exe08.php
@@ -1,1 +1,28 @@
-<?php
+<?php declare(strict_types=1);
+
+class SavingsAccount
+{
+    private float $annualInterestRate;
+    private float $balance;
+
+    public function __construct(float $annualInterestRate, float $balance)
+    {
+        $this->annualInterestRate = $annualInterestRate;
+        $this->balance = $balance;
+    }
+
+    public function withdrawal(float $amount): float
+    {
+        return $this->balance - $amount;
+    }
+
+    public function deposit(float $amount): float
+    {
+        return $this->balance + $amount;
+    }
+
+    public function addInterest(): float
+    {
+        return $this->balance += ($this->balance * $this->annualInterestRate) / 100;
+    }
+}
