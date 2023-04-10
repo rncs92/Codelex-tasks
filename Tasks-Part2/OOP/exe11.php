9198a08898a2df6dbe291ac191e76d8aea5fbfe3
rncs92 <123461096+rncs92@users.noreply.github.com>
2023-04-10 10:59:49 +0300

    update

--- Tasks-Part2/OOP/exe11.php
+++ Tasks-Part2/OOP/exe11.php
@@ -11,26 +11,80 @@ class Account
         $this->balance = $balance;
     }
 
+    public static function transfer(Account $from, Account $to, int $howMuch)
+    {
+        $from->withdrawal($howMuch);
+        $to->deposit($howMuch);
+    }
+
     public function withdrawal(float $amount): float
     {
         return $this->balance -= $amount;
-    }
-
-    public function balance(): float
-    {
-        return $this->balance;
     }
 
     public function deposit(float $amount): float
     {
         return $this->balance += $amount;
     }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function balance(): float
+    {
+        return $this->balance;
+    }
 }
 
-$bartos_account = new Account("Barto's account", 100.00);
-$bartos_swiss_account = new Account("Barto's account in Switzerland", 1000000.00);
+$johnsAccount = new Account("John's account", 100.00);
 
-$bartos_account->withdrawal(20);
-echo "Barto's account balance is now: " . $bartos_account->balance() . PHP_EOL;
-$bartos_swiss_account->deposit(200);
-echo "Barto's Swiss account balance is now: " . $bartos_swiss_account->balance() . PHP_EOL;
+
+echo "Initial state:" . PHP_EOL;
+echo "{$johnsAccount->getName()}, {$johnsAccount->balance()}$" . PHP_EOL;
+
+
+$johnsAccount->deposit(20);
+echo "John's account balance is now: {$johnsAccount->balance()}$" . PHP_EOL;
+
+
+echo "Final state:" . PHP_EOL;
+echo "{$johnsAccount->getName()}, {$johnsAccount->balance()}$" . PHP_EOL;
+
+echo "======================================================" . PHP_EOL;
+echo "Initial state:" . PHP_EOL;
+
+$mattsAccount = new Account("Matt's account", 1000);
+echo "{$mattsAccount->getName()}, {$mattsAccount->balance()}$" . PHP_EOL;
+$mattsAccount->withdrawal(100);
+
+$myAccount = new Account('My account', 0);
+echo "{$myAccount->getName()}, {$myAccount->balance()}$" . PHP_EOL;
+$myAccount->deposit(100);
+
+
+echo PHP_EOL;
+echo "Final state:" . PHP_EOL;
+echo "{$mattsAccount->getName()}, {$mattsAccount->balance()}$" . PHP_EOL;
+echo "{$myAccount->getName()}, {$myAccount->balance()}$" . PHP_EOL;
+echo "======================================================" . PHP_EOL;
+
+$aAccount = new Account('A', 100);
+$bAccount = new Account('B', 0);
+$cAccount = new Account('C', 0);
+echo "Initial state:" . PHP_EOL;
+echo "{$aAccount->getName()}: {$aAccount->balance()}$" . PHP_EOL;
+echo "{$bAccount->getName()}: {$bAccount->balance()}$" . PHP_EOL;
+echo "{$cAccount->getName()}: {$cAccount->balance()}$" . PHP_EOL;
+
+Account::transfer($aAccount, $bAccount, 50);
+Account::transfer($bAccount, $cAccount, 25);
+
+
+echo "Final state:" . PHP_EOL;
+echo "{$aAccount->getName()}: {$aAccount->balance()}$" . PHP_EOL;
+echo "{$bAccount->getName()}: {$bAccount->balance()}$" . PHP_EOL;
+echo "{$cAccount->getName()}: {$cAccount->balance()}$" . PHP_EOL;
+echo "======================================================" . PHP_EOL;
+
