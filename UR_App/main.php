86f9a4bf8497abb1458853066c16bd48be743a02
rncs92 <123461096+rncs92@users.noreply.github.com>
2023-04-17 16:24:39 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	WeatherReport/vendor/composer/ClassLoader.php

--- UR_App/main.php
+++ UR_App/main.php
@@ -6,14 +6,6 @@ use App\ApiClient;
 use App\Application;
 use App\Registry;
 
-/*
-$client = new ApiClient();
-//$client->getData();
-//$registry = new \App\UznemRegistrs($client);
-//$registry->addBusiness();
-$app = new Application($client);
-$app->run();
-*/
 
 $apiClient = new ApiClient();
 $registry = new Registry($apiClient->getData());
--- UR_App/src/Registry.php
+++ UR_App/src/Registry.php
@@ -10,15 +10,7 @@ class Registry
 
     public function __construct(array $businesses = [])
     {
-        foreach ($businesses as $business) {
-            $this->addBusiness();
-        }
         $this->businesses = $businesses;
-    }
-
-    public function addBusiness(): void
-    {
-
     }
 
     public function getBusinesses(): array
@@ -26,25 +18,6 @@ class Registry
         return $this->businesses;
     }
 
-    public function business(): Business
-    {
-        foreach ($this->businesses as $business) {
-            $name = $business->name;
-            $regcode = $business->regcode;
-            $type = $business->type;
-            $address = $business->address;
-            $registered = $business->registered;
-            $terminated = $business->terminated;
-        }
-
-        return new Business(
-            $name,
-            $regcode,
-            $type,
-            $address,
-            $registered,
-            $terminated);
-    }
 }
 
 
